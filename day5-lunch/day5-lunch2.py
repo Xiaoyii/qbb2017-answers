8d4aa039c52f8cd1a90954473afa106b7f353a97
Xiaoyi Li <xli171@jhu.edu>
2017-09-01 16:32:06 -0400

    part 4 statsmodel OLS

--- day5-lunch/day5-lunch2.py
+++ day5-lunch/day5-lunch2.py
@@ -11,19 +11,22 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 
+#access the H3K...tab files we made using bigWigAverageOverBed and read them, keep their index numbers
 h3k27me3 = pd.read_csv( "H3K27me3.tab", sep = "\t", header=None, index_col=0 )
 h3k36me3 = pd.read_csv( "H3K36me3.tab", sep = "\t", header=None, index_col=0 )
 h3k4me3 = pd.read_csv( "H3K4me3.tab", sep = "\t", header=None, index_col=0 )
 h3k9me3 = pd.read_csv( "H3K9me3.tab", sep = "\t", header=None, index_col=0 )
 
+#access and open the ctab file with FPKM, move the index over to here
 df_ctab = pd.read_csv( sys.argv[1], sep = "\t", index_col="t_name" )
 
-
+#the average is in column 5
 h3k27me3_ave = h3k27me3[5]
 h3k36me3_ave = h3k36me3[5]
 h3k4me3_ave = h3k4me3[5]
 h3k9me3_ave = h3k9me3[5]
 
+#concatinate the 4 samples and the FPKMs that we want to compare into one pd file
 h3k = pd.concat((h3k27me3_ave, h3k36me3_ave, h3k4me3_ave, h3k9me3_ave, \
 df_ctab["FPKM"]), 1, join="inner")
 
@@ -31,6 +34,7 @@ df_ctab["FPKM"]), 1, join="inner")
 
 #print "next one"
 
+#calls for statsmodel OLS
 model = sm.OLS(h3k["FPKM"], h3k.iloc[:,:4])
 results = model.fit()
 print (results.summary())
